6b2adef93cfb86258cb2f4bc7c7b827423d45585
Horia Mercan <horia.mercan@gmail.com>
2024-12-30 00:35:12 +0200

    added tracker.h

---
+++ src/tracker.h
@@ -0,0 +1,35 @@
+#ifndef __TRACKER_H__
+#define __TRACKER_H__
+
+#include <unordered_map>
+#include <unordered_set>
+#include <string>
+#include <vector>
+#include <mpi.h>
+
+#include "file_hash.h"
+#include "client.h"
+
+
+class Tracker: public Entity {
+public:
+    Tracker(int numProcs);
+    ~Tracker();
+
+    int ReceiveInfoFromClient();
+
+    std::unordered_map<std::string, std::vector<int>> file_to_seeds;
+    std::unordered_map<std::string, std::unordered_set<int>> file_to_peers;
+
+    void ServeRequests();
+
+    void StopUploadingClients();
+
+protected:
+    std::unordered_map<std::string, std::pair<int, FileHash *>> file_to_hashes;
+    
+};
+
+
+
+#endif  /* __TRACKER_H__ */
